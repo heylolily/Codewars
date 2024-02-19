43ad0890ece9bdc3257b53364704516d8a82b9ab
Lily <106398102+heylolily@users.noreply.github.com>
2024-02-19 14:17:40 -0600

    coding challenge for 2024-02-19 7kyu

---
+++ CC 2024-02-19/digitalcypher.js
@@ -0,0 +1,57 @@
+// Introduction
+// Digital Cypher assigns to each letter of the alphabet unique number. For example:
+
+//  a  b  c  d  e  f  g  h  i  j  k  l  m
+//  1  2  3  4  5  6  7  8  9 10 11 12 13
+//  n  o  p  q  r  s  t  u  v  w  x  y  z
+// 14 15 16 17 18 19 20 21 22 23 24 25 26
+// Instead of letters in encrypted word we write the corresponding number, eg. The word scout:
+
+//  s  c  o  u  t
+// 19  3 15 21 20
+// Then we add to each obtained digit consecutive digits from the key. For example. In case of key equal to 1939 :
+
+//    s  c  o  u  t
+//   19  3 15 21 20
+//  + 1  9  3  9  1
+//  ---------------
+//   20 12 18 30 21
+  
+//    m  a  s  t  e  r  p  i  e  c  e
+//   13  1 19 20  5 18 16  9  5  3  5
+// +  1  9  3  9  1  9  3  9  1  9  3
+//   --------------------------------
+//   14 10 22 29  6 27 19 18  6  12 8
+// Task
+// Write a function that accepts str string and key number and returns an array of integers representing encoded str.
+
+// Input / Output
+// The str input string consists of lowercase characters only.
+// The key input number is a positive integer.
+
+// Example
+// Encode("scout",1939);  ==>  [ 20, 12, 18, 30, 21]
+// Encode("masterpiece",1939);  ==>  [ 14, 10, 22, 29, 6, 27, 19, 18, 6, 12, 8]
+
+
+function encode(str,  n) {
+  const letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  let key = n.toString().split('').map(x => Number(x))
+  let code = str.split('').map(x => letters.indexOf(x)+1)
+  // this approach uses 2 separate arrays, one for the string, and one for the key. the key is first made to be equal length to the code and then proceeds
+
+  key = code.map((x, i) => key[i % key.length])
+  
+  code = code.map((x,ind) => x + key[ind])
+  
+  return code
+}
+
+// condensed approach using ascii codes using charCodeAt()
+
+function encode(str, n) {
+    const key = String(n)
+    // this one skips making the key array the same length as the string, but still utilizes the same method to find the right index
+    return str.split('').map((c, i) => c.charCodeAt(0) - 96 + Number(key[i % key.length]))
+    // return Array.from(str, (c, i) => c.charCodeAt(0) - 96 + Number(key[i % key.length])) is the same line as above
+  }
