8cda9387a924513da241dc75db3f0e3476884e85
Lily <106398102+heylolily@users.noreply.github.com>
2024-02-24 21:46:26 -0600

    coding challenge 6kyu! working with arrays is getting easier. just unlucky that the code i wanted to work with didnt work

---
+++ stopspinningmywords.js
@@ -0,0 +1,20 @@
+// Write a function that takes in a string of one or more words, and returns the same string, but with all words that have five or more letters reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+// Examples:
+
+// "Hey fellow warriors"  --> "Hey wollef sroirraw" 
+// "This is a test        --> "This is a test" 
+// "This is another test" --> "This is rehtona test"
+
+
+function spinWords(string){
+    return string.split(' ').map(x => x.length >= 5 ? x.split('').reverse().join('') : x).join(' ')
+  }
+
+
+
+// i like this solution because it uses regex to replace the words with backwards versions. 
+
+function spinWords(string){
+    return string.replace(/\w{5,}/g, function(w) { return w.split('').reverse().join('') })
+  }
