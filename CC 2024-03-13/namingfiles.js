b41d7634118c124040b8b8a66f93923d196e5145
Lily <106398102+heylolily@users.noreply.github.com>
2024-03-13 15:35:21 -0500

    catching up on more katas. 7kyu

---
+++ CC 2024-03-13/namingfiles.js
@@ -0,0 +1,43 @@
+// Naming multiple files can be a pain sometimes.
+
+// Task:
+// Your job here is to create a function that will take three parameters, fmt, nbr and start, and create an array of nbr elements formatted according to frm with the starting index start. fmt will have <index_no> inserted at various locations; this is where the file index number goes in each file.
+
+// Description of edge cases:
+// If nbr is less than or equal to 0, or not whole, return an empty array.
+// If fmt does not contain '<index_no>', just return an array with nbr elements that are all equal to fmt.
+// If start is not an integer, return an empty array.
+// What each parameter looks like:
+// typeof frm #=> 'string'
+//   : "text_to_stay_constant_from_file_to_file <index_no>"
+// typeof nbr #=> 'number'
+//   : number_of_files
+// typeof start #=> 'number'
+//   : index_no_of_first_file
+// typeof (nameFile(frm, nbr, start)) #=> 'array'
+// Some examples:
+// nameFile("IMG <index_no>", 4, 1)
+//   #=> ["IMG 1", "IMG 2", "IMG 3", "IMG 4"])
+// nameFile("image #<index_no>.jpg", 3, 7)
+//   #=> ["image #7.jpg", "image #8.jpg", "image #9.jpg"]
+// nameFile("#<index_no> #<index_no>", 3, -2)
+//   #=> ["#-2 #-2", "#-1 #-1", "#0 #0"]
+
+// there has to be a way to condense this. i wanted to use Array
+function nameFile(fmt, nbr, start) {
+    if (!Number.isInteger(start) || nbr < 1) {
+        return []
+    }
+    let arr = []
+    for (let i = start; i < nbr+start; i++) {
+        let file = fmt.replaceAll('<index_no>', i)
+        arr.push(`${file}`)
+    }
+    return arr
+    
+}
+
+// condesned into a one liner
+function nameFile(fmt, nbr, start) {
+    return !Number.isInteger(start) || nbr < 1 ? [] : Array(nbr).fill(fmt).map((x, i) => x.replace('<index_no>', start + i));
+}
