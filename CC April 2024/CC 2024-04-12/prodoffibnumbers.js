c8d2609ff355d8a8505ee1c35087dbad4faa2668
Lily <106398102+heylolily@users.noreply.github.com>
2024-04-12 14:57:54 -0500

    5kyu

---
+++ CC April 2024/CC 2024-04-12/prodoffibnumbers.js
@@ -0,0 +1,77 @@
+// The Fibonacci numbers are the numbers in the following integer sequence (Fn):
+
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
+
+// such as
+
+// F(n) = F(n-1) + F(n-2) with F(0) = 0 and F(1) = 1.
+
+// Given a number, say prod (for product), we search two Fibonacci numbers F(n) and F(n+1) verifying
+
+// F(n) * F(n+1) = prod.
+
+// Your function productFib takes an integer (prod) and returns an array:
+
+// [F(n), F(n+1), true] or {F(n), F(n+1), 1} or (F(n), F(n+1), True)
+// depending on the language if F(n) * F(n+1) = prod.
+
+// If you don't find two consecutive F(n) verifying F(n) * F(n+1) = prodyou will return
+
+// [F(n), F(n+1), false] or {F(n), F(n+1), 0} or (F(n), F(n+1), False)
+// F(n) being the smallest one such as F(n) * F(n+1) > prod.
+
+// Some Examples of Return:
+// (depend on the language)
+
+// productFib(714) # should return (21, 34, true), 
+//                 # since F(8) = 21, F(9) = 34 and 714 = 21 * 34
+
+// productFib(800) # should return (34, 55, false), 
+//                 # since F(8) = 21, F(9) = 34, F(10) = 55 and 21 * 34 < 800 < 34 * 55
+// -----
+// productFib(714) # should return [21, 34, true], 
+// productFib(800) # should return [34, 55, false], 
+// -----
+// productFib(714) # should return {21, 34, 1}, 
+// productFib(800) # should return {34, 55, 0},        
+// -----
+// productFib(714) # should return {21, 34, true}, 
+// productFib(800) # should return {34, 55, false}, 
+// Note:
+// You can see examples for your language in "Sample Tests".
+
+
+// not gonna lie, the instructions were a bit hard to understand at first glance, but I basically needed to find the two consecutive fib numbers whos product matched the given variable. if there are 2 numbers, return them with true at the end of the array, and if not, false
+
+
+// my solution
+function productFib(prod){
+    let fib = [0,1]
+    let total = 1
+    while (total < prod) {
+      let next = fib[0] + fib[1]
+      fib[0] = fib[1]
+      fib[1] = next
+      total = fib[0]*fib[1]
+    }
+    fib.push(prod === total)
+    return fib
+  }
+
+// alternative that does the same methods as mine
+function productFib(prod){
+    var n = 0;
+    var nPlus = 1;  
+    while(n*nPlus < prod) {
+      nPlus = n + nPlus;
+      n = nPlus - n;
+    }
+    return [n, nPlus, n*nPlus===prod];
+  }
+
+//another alternative solution. i like this because it doesn't require me to make another variable for the next fib numbers. i always forget i can declare 2 variables in one like like this. its good to remember for future instances
+  function productFib(prod){
+    let [a, b] = [0, 1];
+    while(a * b < prod) [a, b] = [b, a + b];
+    return [a, b, a * b === prod];
+  }
